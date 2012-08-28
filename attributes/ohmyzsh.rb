6a66eb0f3f2d3318d89f1b25c9926f80a8aeddcb
Cameron Johnston <cameron@needle.com>
2012-08-28 09:50:37 -0600

    making the source repository configurable
    Personally, I prefer Sorin Ionescu's fork.

--- attributes/ohmyzsh.rb
+++ attributes/ohmyzsh.rb
@@ -1,1 +1,2 @@
+default[:ohmyzsh][:repo] = "https://github.com/robbyrussell/oh-my-zsh.git"
 default[:ohmyzsh][:theme] = "afowler"
--- recipes/default.rb
+++ recipes/default.rb
@@ -24,7 +24,7 @@ search( :users, "shell:*zsh" ).each do |u|
   user_id = u["id"]
 
   git "/home/#{user_id}/.oh-my-zsh" do
-    repository "https://github.com/robbyrussell/oh-my-zsh.git"
+    repository node[:ohmyzsh][:repo]
     reference "master"
     user user_id
     group user_id
