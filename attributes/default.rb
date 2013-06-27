acd6ed33ea5962d40bb2627ff83b0d01f7292bff
Leif Ringstad <leifcr@gmail.com>
2013-06-27 14:55:34 +0200

    Dont use dot expansion, (annoying using log:....... in runit)

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@ default[:prezto][:repo]           = "https://github.com/sorin-ionescu/prezto.git
 default[:prezto][:theme]          = "nicoulaj"
 default[:prezto][:prezto_modules] = %w('environment' 'terminal' 'editor' 'history' 'directory' 'spectrum' 'utility' 'completion' 'prompt' 'command-not-found' 'dpkg')
 default[:prezto][:editor]         = "emacs"
-default[:prezto][:dotexpansion]   = "yes"
+default[:prezto][:dotexpansion]   = "no"
 default[:prezto][:autotitle]      = "yes"
