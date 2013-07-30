145bf5dd9cefa3fe4d3cfef3b8a40175d1acac8c
Leif Ringstad <leifcr@gmail.com>
2013-07-30 16:02:53 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "leifcr@gmail.com"
 license          "MIT"
 description      "Installs/Configures prezto"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
 
 depends          "git"
 depends          "zsh"
